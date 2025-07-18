8aeabbe2cd652de72b50cd3f1489f92bc385b92e
Matthew Muniz <48893316+munizio@users.noreply.github.com>
2025-07-17 21:01:22 -0500

    Delete install/webapps.sh

--- install/webapps.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -z "$OMARCHY_BARE" ]; then
-  source ~/.local/share/omarchy/default/bash/functions
-  web2app "YouTube" https://youtube.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/youtube.png
-  web2app "GitHub" https://github.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/github-light.png
-  web2app "X" https://x.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/x-light.png
-fi
